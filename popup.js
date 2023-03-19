15fb73c4096badd72c63ce962b9c063a4a89025d
GosbyQin <35714942+GosbyQin@users.noreply.github.com>
2023-03-19 20:05:04 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,13 @@
 // 获取页面元素
 const fileInput = document.getElementById('file-input');
 const imageOutput = document.getElementById('image-output');
+const imageUpload = document.getElementById('image-upload');
 const base64InputBox = document.getElementById('base64-input-box');
 const base64Output = document.getElementById('base64-output');
 const convertButton = document.getElementById('convert-button');
 const imagePreview = document.getElementById('image-preview');
 const imageSize = document.getElementById('image-size');
+const textarea = document.getElementById('image-output');
 
 // 监听文件输入框的 change 事件
 fileInput.addEventListener('change', (event) => {
@@ -29,6 +31,20 @@ fileInput.addEventListener('change', (event) => {
     alert('请选择图片文件');
   }
 });
+
+// 从 URL 加载图片
+imageOutput.addEventListener('input', () => {
+  const imageUrl = imageOutput.value;
+  if (isValidImageUrl(imageUrl)) {
+    const imgElement = document.createElement('img');
+    imgElement.src = imageUrl;
+    imageUpload.appendChild(imgElement);
+  }
+});
+
+function isValidImageUrl(url) {
+  return (url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+}
 
 // 监听粘贴事件
 document.addEventListener('paste', (event) => {
@@ -63,12 +79,81 @@ convertButton.addEventListener('click', () => {
     const base64 = imageOutput.src.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
     base64InputBox.value = base64;
   }
-  if (base64InputBox.value) {
-    base64Output.src = `data:image/png;base64,${base64InputBox.value}`;
-    base64Output.style.display = 'block';
+
+  // 判断是否有输入图片 URL
+  const imageUrl = imageOutput.value.trim();
+  if (imageUrl) {
     const img = new Image();
-    img.src = base64Output.src;
+    img.crossOrigin = 'anonymous';
+    img.src = imageUrl;
+
+    // 监听图片加载完成事件
+    img.addEventListener('load', () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0, img.width, img.height);
+
+      // 转换为 Base64
+      const base64 = canvas.toDataURL('image/png').replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
+      base64InputBox.value = base64;
+
+      // 显示转换后的图片和原图片
+      base64Output.src = `data:image/png;base64,${base64InputBox.value}`;
+      base64Output.style.display = 'block';
+      imageOutput.src = imageUrl;
+      imageOutput.style.display = 'block';
+
+      // 更新图片大小显示
+      if (imageSize !== null) {
+        imageSize.innerHTML = `${(base64InputBox.value.length * 0.75 / 1024).toFixed(2)} KB`;
+      }
+    });
+
+    // 监听图片加载失败事件
+    img.addEventListener('error', () => {
+      console.error('Failed to load image from URL:', imageUrl);
+    });
+  }
+
+function previewImageFromUrl(url) {
+  fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      const base64 = data.split(',')[1];
+      imageOutput.src = `data:image/png;base64,${base64}`;
+      imageOutput.style.display = 'block';
+    })
+    .catch(error => {
+      console.error('Failed to fetch image from URL:', error);
+    });
+}
+
+textarea.addEventListener('input', () => {
+  const url = textarea.value.trim();
+  if (url.startsWith('data:image/')) {
+    // If the input is already a Base64 image, just display it
+    imageOutput.src = url;
+    imageOutput.style.display = 'block';
+  } else if (url.startsWith('http')) {
+    // If the input is a URL, fetch the image and display it
+    previewImageFromUrl(url);
+  } else {
+    // Otherwise, clear the image preview
+    imageOutput.src = '';
+    imageOutput.style.display = 'none';
+  }
+});
+
+  // 如果有输入的 Base64，则将其转换为图片并显示
+  if (base64InputBox.value) {
+    const img = new Image();
+    img.src = `data:image/png;base64,${base64InputBox.value}`;
     img.onload = () => {
+      base64Output.src = img.src;
+      base64Output.style.display = 'block';
       imageOutput.src = img.src;
       imageOutput.style.display = 'block';
       if (imageSize !== null) {
